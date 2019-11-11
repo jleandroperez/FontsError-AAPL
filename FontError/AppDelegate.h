a5313720b84b0aec46862b2b8828f99aa951baf5
Jorge Leandro Perez <jorge.perez@automattic.com>
2019-11-11 15:21:17 -0300

    Initial commit

---
+++ FontError/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FontError
+//
+//  Created by Lantean on 11/6/19.
+//  Copyright © 2019 A8C. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
